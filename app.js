29da1691e7ccc342ce75d2904031b39228723fc6
claudiofepereira <claudiopereira@ipvc.pt>
2020-10-27 17:31:13 +0000

    Added custom grid on user input, clear grid button, grid coloring

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
-//Sets important constants and variables
+const container = document.querySelector('.container');
+const setGridBtn = document.querySelector('.set-grid');
+const clearGridBtn = document.querySelector('.clear-grid');;
 
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+let newGridSize = 0;
 
-
-defaultGrid();
-//Creates a default grid sized 16x16 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+// This function simply creates a grid by manipulating css
+function makeGrid(num = 16) {
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
 }
 
-//Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-    //Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
+// Receives input from user and defines global grid size
+function changeGridSize() {
+    newGridSize = parseInt(prompt("Enter a number:", "Needs to be in range (2,100)"));
+    if (newGridSize < 2 || newGridSize > 100 || !newGridSize) {
+        newGridSize = 16;
+    } else {
+        container.innerHTML = "";
+        createDivs(newGridSize);
+        addMouseOverToGrids();
+        makeGrid(newGridSize);
+    }
+}
 
-//Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
+// Uses global grid size defined in changeGridSize() from user
+// If there's no grid size defined from user, use 16 as def
+function clearGrid() {
+    if (newGridSize < 2 || newGridSize > 100 || !newGridSize) {
+        newGridSize = 16;
+    } else {
+        container.innerHTML = "";
+        createDivs(newGridSize);
+        addMouseOverToGrids();
+        makeGrid(newGridSize);
+    }
+}
 
-    };
-};
+function createDivs(gridSize = 16) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let div = document.createElement("div");
+        div.classList.add('grid');
+        container.append(div);
+    }
+}
+
+// Function to change the color with which the grid cells will be "painted"
+function changePencilColor(color) {
+    return function(e) {
+        e.target.style.backgroundColor = color;
+    }
+}
+
+// Function to color the grid cells when mouse passes over them
+function addMouseOverToGrids() {
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach(element => {
+        element.onmouseover = changePencilColor('rgb(0,0,0)');
+    });
+}
+
+// This is the absolute main function, it compiles all functions into it
+function init() {
+    createDivs();
+    addMouseOverToGrids();
+    makeGrid();
+}
+
+setGridBtn.addEventListener('click', function() {
+    changeGridSize();
+});
+
+clearGridBtn.addEventListener('click', function() {
+    clearGrid();
+})
+
+window.addEventListener('load', init());
